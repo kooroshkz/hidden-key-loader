a039d9d1145dfb69ada4b52cd7fc5d1079475be7
Koorosh Ubuntu <kkomeilizadeh@gmail.com>
2024-02-07 09:30:01 +0100

    File object created and Dry codes removed

--- credentials.py
+++ credentials.py
@@ -1,16 +1,22 @@
-try:
-    with open('dev_credentials.txt', 'r') as file:
+class File():
+    def __init__(self):
+        self.dev_file = "dev_credentials.txt"
+        self.file = "credentials.txt"
+        self.final = None
+        try :
+            with open(self.dev_file, 'r') as file:
+                self.final = self.dev_file
+        
+        except FileNotFoundError:
+            with open(self.file, 'r') as file:
+                self.final = self.file
 
-        for line in file:
-            variable, value = line.strip().split(' = ')
-            value = value.strip('"')
-            exec(f'{variable} = "{value}"')
-
-except FileNotFoundError:
-
-    with open('credentials.txt', 'r') as file:
-
-        for line in file:
-            variable, value = line.strip().split(' = ')
-            value = value.strip('"')
-            exec(f'{variable} = "{value}"')
+    def fetch_data(self):
+        with open(self.final, 'r') as file:
+            for line in file:
+                variable, value = line.strip().split(' = ')
+                value = value.strip('"')
+                exec(f'global {variable}; {variable} = "{value}"')
+        
+file_object = File()
+file_object.fetch_data()
